13bee8321e44dabc997f4228b5cdfc51b6ffb593
Maciej Sawicki <sawik11@wp.pl>
2018-02-25 21:39:08 +0100

    Added api maven project starter

---
+++ api/src/main/kotlin/pl/humberd/teamclickerapi/TeamClickerApiApplication.kt
@@ -0,0 +1,11 @@
+package pl.humberd.teamclickerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamClickerApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamClickerApiApplication>(*args)
+}
---
+++ api/src/test/kotlin/pl/humberd/teamclickerapi/TeamClickerApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package pl.humberd.teamclickerapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TeamClickerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
